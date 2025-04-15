0b74d58540156f8e56ae6223dd195a50c1678797
Matheus Barreto <matheus.h.barreto6@aluno.senai.br>
2025-04-15 08:48:56 -0300

    Exercícios

--- Exercícios/Ex08_05.js
+++ Exercícios/Ex08_05.js
@@ -1,19 +1,6 @@
-//Receba uma palavra do usuário e calcule quantas vogais há nesta palavra.
-const prompt = require('prompt-sync')();
-let vogais = ['a', 'e', 'i', 'o', 'u']
+let numeros = Math.floor(Math.random() * 100) + 1;
 
 
-let palavra = prompt("Digite uma palavra:");
-
-
-
-// Conta as vogais
-let contador = 0;
-for (let letra of palavra.toLowerCase()) {
-  if (vogais.includes(letra)) {
-    contador++;
-  }
-}
-
-// Exibe o resultado
-console.log(`A palavra "${palavra}" contém ${contador} vogais.`);
+numeros.forEach(numeros => {
+  console.log(`Número: ${numeros}, Dobro: ${numeros * 2}`);
+});
